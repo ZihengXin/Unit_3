565bb506e9adae0493597b26f08645e4657585e2
Starmicks <ziheng.xin@ssfs.org>
2019-10-18 15:10:03 -0400

    10/18 String References

--- src/StringReferences.java
+++ src/StringReferences.java
@@ -1,8 +1,12 @@
+// 10/18 Charly. This program is testing the function of == and !=
 public class StringReferences {
     public static void main(String[] args){
         String word = "hello";
         String sentence = null;
         String str = "hello";
-        System.out.println("");
+        System.out.println(word == sentence);
+        System.out.println(sentence == null);
+        System.out.println(word == str);
+        System.out.println(word != null);
     }
 }
