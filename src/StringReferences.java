5882649685ef1ff7464c5354d62eb8ffad1ef3d9
xinzi <ziheng.xin@ssfs.org>
2019-10-18 11:09:35 -0400

    1018 string(not done)

---
+++ src/StringReferences.java
@@ -0,0 +1,8 @@
+public class StringReferences {
+    public static void main(String[] args){
+        String word = "hello";
+        String sentence = null;
+        String str = "hello";
+        System.out.println("");
+    }
+}
