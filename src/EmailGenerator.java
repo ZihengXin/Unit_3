d151becddac7cc0d94fb576d9fd08c5e574ea071
Starmicks <ziheng.xin@ssfs.org>
2019-11-01 14:59:17 -0400

    11/1 EmailGenerator(done)

--- src/EmailGenerator.java
+++ src/EmailGenerator.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 public class EmailGenerator {
     public static void main(String[] args) {
         System.out.println("Enter your First and Last name: ");
-        // Please enter the first name and last name in the same line. 
+        // Please enter the first name and last name in the same line.
         Scanner input= new Scanner(System.in);
         String name= input.nextLine();
         int space= name.indexOf(" ");
         String firstName= name.substring(0,space);
         String lastName= name.substring(space+1);
         System.out.print("This user’s e-mail is: ");
-        String part1 = EmailGenerator.makeUserName(firstName,lastName);
-        String part2 = EmailGenerator.makeEmail();
+        String part1= EmailGenerator.makeUserName(firstName,lastName);
+        String part2= EmailGenerator.makeEmail();
         System.out.print(part1+part2);
     }
     public static String makeUserName(String firstName,String lastName){
