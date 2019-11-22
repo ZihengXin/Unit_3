52c72a3bf6aafbb15e7180df5ccd21a3f0ef04e4
Starmicks <ziheng.xin@ssfs.org>
2019-11-22 14:39:13 -0500

    11/22 RightTriangle

--- src/RightTriangle.java
+++ src/RightTriangle.java
@@ -1,23 +1,28 @@
-// 10/30 Charly. This program should be able to tell three length can form a right triangle or not.
+// 11/22 Charly. This program should be able to tell three length can form a right triangle or not.
+// I have just correct my program into the form you want. 
 import java.util.Scanner;
 
 public class RightTriangle {
+    public static boolean isTriangle(int a, int b, int c){
+        return a+b>c && b+c>a && a+c > b;
+    }
+
+    public static boolean isRight(int a, int b, int c){
+        return Math.pow(a,2) + Math.pow(b,2) == Math.pow(c,2);
+    }
     public static void main(String[] args){
         System.out.println("Enter three integer sides of a triangle: ");
         Scanner input= new Scanner(System.in);
-        int lengthOne= input.nextInt();
-        int lengthTwo= input.nextInt();
-        int lengthThree= input.nextInt();
-        System.out.print(isRight(lengthOne,lengthTwo,lengthThree));
-    }
-    public static String isRight(int a, int b, int c){
-        if (((a+b)>c)&&((a+c)>b)&&((b+c)>a)){
-            if((a*a+b*b==c*c)||(a*a+c*c==b*b)||(b*b+c*c==a*a))
-                return "The side lengths of "+a+", "+b+", and "+c+" form a right triangle";
-            else
-                return "The side lengths of "+a+", "+b+", and "+c+" form a  triangle, but it is not a right triangle";
-        }
+        int a= input.nextInt();
+        int b= input.nextInt();
+        int c= input.nextInt();
+        System.out.print(isRight(a,b,c));
+
+        if(isTriangle(a, b, c) && isRight(a, b, c))
+            System.out.println("The side lengths of "+a+", "+b+", and "+c+" form a right triangle");
+        else if(isTriangle(a, b, c) && !isRight(a, b, c))
+            System.out.println("The side lengths of "+a+", "+b+", and "+c+" form a  triangle, but it is not a right triangle");
         else
-            return "The side lengths of "+a+", "+b+", and "+c+" do not form a triangle";
+            System.out.println("The side lengths of "+a+", "+b+", and "+c+" do not form a triangle");
     }
 }
